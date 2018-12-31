ce31932fd15987b60809dda49f932612ef8dec9e
kvgrazier <kvgrazier@hotmail.com>
2018-12-30 19:37:25 -0500

    c

--- src/app/account-create/account-create.component.ts
+++ src/app/account-create/account-create.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account-create',
   templateUrl: './account-create.component.html'
 })
 export class AccountCreateComponent implements OnInit {
+  accountForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.accountForm = this.formBuilder.group({
+      AccountNumber : [null],
+      AccountName : [null, Validators.required],
+      TaxFormRef : [null, Validators.required]
+      ,Active : [null, Validators.required]
+      ,'AccountPerson.AccountPersonName' : []
+      ,'AccountSubType.AccountSubTypeName' : [null, Validators.required]
+      ,'AccountSubType.SortOrder' : [null, Validators.required]
+      ,'AccountSubType.AccountType.AccountTypeName' : [null]
+    });
   }
 
+  onFormSubmit(form:NgForm) {
+    this.api.postAccount(form)
+      .subscribe(res => {
+          let id = res['_id'];
+          this.router.navigate(['/account-details', id], { queryParamsHandling: 'preserve' });
+        }, (err) => {
+          console.log(err);
+        });
+  }
 }
